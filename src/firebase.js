a3fe39ceef648981143614bea7de07d21ae81dda
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-16 11:02:55 +0530

    completed

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "process.env.api_key",
+  authDomain: "sachu-just-clock-it.firebaseapp.com",
+  databaseURL: "https://sachu-just-clock-it.firebaseio.com",
+  projectId: "sachu-just-clock-it",
+  storageBucket: "sachu-just-clock-it.appspot.com",
+  messagingSenderId: "process.env.messagingSenderId",
+  appId: "process.env.appId",
+  measurementId: "process.env.measurementId",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
